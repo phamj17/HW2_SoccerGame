4c0fa6c49a136905b0bd2efd4c53c85aca81cfca
phamj17 <phamj17@up.edu>
2015-09-28 23:50:05 -0700

    Layout finished commit.

--- app/src/main/java/com/example/administrator/hw2_soccergame/MainMenuActivity.java
+++ app/src/main/java/com/example/administrator/hw2_soccergame/MainMenuActivity.java
@@ -1,17 +1,54 @@
 package com.example.administrator.hw2_soccergame;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainMenuActivity extends ActionBarActivity {
+public class MainMenuActivity extends ActionBarActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
+
+    private SoccerDB database;
+
+    private Button toPlayerActivity;
+    private Spinner teams_spinner;
+    private ArrayList<String> teamNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        toPlayerActivity = (Button) findViewById(R.id.toPlayerActivity_button);
+        toPlayerActivity.setOnClickListener(this);
+
+        teams_spinner = (Spinner) findViewById(R.id.teams_spinner);
+
+        teamNames = new ArrayList<String>();
+        teamNames.add("Team One");
+        teamNames.add("Team Two");
+        teamNames.add("Team Three");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, teamNames);
+
+        // Drop down layout style - list view with radio button
+        //dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        teams_spinner.setAdapter(dataAdapter);
+        teams_spinner.setOnItemSelectedListener(this);
+
     }
 
     @Override
@@ -34,5 +71,36 @@ public class MainMenuActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == toPlayerActivity){
+            startActivity(new Intent(MainMenuActivity.this, PlayerActivity.class));
+        }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
